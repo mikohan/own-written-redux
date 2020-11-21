97a9f96f41f4b153f044a215855fe213ff53a77f
mikohan <angara99@gmail.com>
2020-11-21 15:19:12 +0300

    Added index html

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import './styles/index.scss';
-import { MyClass } from '~/newDelete';
-// import '../assets/favicon.png';
 
-console.log('Typescript Simple Webpack Config');
+const counter = document.getElementById('counter') as HTMLElement;
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
 
-const my = new MyClass();
+function render() {
+  counter.textContent = state.toString();
+}
 
-my.setName('Vlad');
-my.setYear(1971);
+let state: number = 0;
 
-console.log(my.getAll);
+addBtn?.addEventListener('click', () => {
+  state = state + 1;
+  render();
+});
+
+subBtn?.addEventListener('click', () => {
+  state = state - 1;
+  render();
+});
+
+themeBtn?.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
+
+asyncBtn?.addEventListener('click', () => {
+  setTimeout(() => {
+    state = state + 1;
+    render();
+  }, 2000);
+});
